f3397750083f818ee2a8c661b82ee25d640898ca
btflp <runsinn976@gmail.com>
2025-06-03 07:40:05 +0000

    wip

--- build_osmand.sh
+++ build_osmand.sh
@@ -17,13 +17,13 @@ if [ ! -d "/tmp/android-sdk" ]; then
     fi
 fi
 
-if [ ! -d "/workspaces/temp/osmand/android" ]; then
+if [ ! -d "/workspaces/temp/android" ]; then
     curl https://storage.googleapis.com/git-repo-downloads/repo > ./repo
     chmod a+x ./repo
     ./repo init -u https://github.com/osmandapp/OsmAnd-manifest -m readonly.xml
     ./repo sync
 
-    cd /workspaces/temp/osmand/android
+    cd /workspaces/temp/android
     sed -e 's#storeFile file("/var/lib/jenkins/osmand_key")#storeFile file("../keystores/debug.keystore")#' \
     -e 's#storePassword System.getenv("OSMAND_APK_PASSWORD")#storePassword "android"#' \
     -e 's#storePassword System.getenv("OSMAND_APK_PASSWORD")#storePassword "android"#' \
@@ -33,6 +33,9 @@ if [ ! -d "/workspaces/temp/osmand/android" ]; then
     mv OsmAnd/build.gradle.neu OsmAnd/build.gradle    
 fi
 
-cd /workspaces/temp/osmand/android
+cd /workspaces/temp/android
+
+#git ../patch_7dd385fd62.diff
 JAVA_TOOL_OPTIONS=' -Xmx10g' JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64 PATH=$JAVA_HOME/bin:$PATH ANDROID_HOME=/tmp/android-sdk PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin ./gradlew assembleAndroidFullLegacyArm64
+
 exit 0
